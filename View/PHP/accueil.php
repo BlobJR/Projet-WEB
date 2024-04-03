654323b95354b1d8e33b10a9aa0dc823bec49eb8
BlobJR <alexandreroussel64@gmail.com>
2024-04-03 16:30:36 +0200

    recherche etudiant

--- View/PHP/accueil.php
+++ View/PHP/accueil.php
@@ -1,7 +1,12 @@
 <?php
-require_once'../../Controller/controlaccueil.php';
+require_once '../../Model/modelacceuil.php';
 require_once 'connexiondb.php';
-$offres=insertoff($pdo);
+
+$offres = getoff($pdo);
+$villes = getVille($pdo);
+$secteurs = getsecteurs($pdo);
+$comp_requises = getcomp($pdo);
+$entreprises = getNomEntreprises($pdo); 
 ?>
 
 <!DOCTYPE html>
@@ -25,7 +30,7 @@ $offres=insertoff($pdo);
             </a>
         </div>
         <div class="account">
-            <a class="account" href="Connexion.php">COMPTE</a>
+            <a class="account" href="dashboard.html">COMPTE</a>
         </div>
 
     </header>
@@ -33,60 +38,61 @@ $offres=insertoff($pdo);
     <section class="container">
 
         <section class="top"> 
-
-            <section class="criteres">
-            
-                <div class="search">
-                    <input placeholder="Recherche" class="search" type="text" />
-                    <button type="submit" class="search-button">
-                        <img src="../img/loupe.png" alt="Recherche">
-                    </button>
-                </div>
-                
-                
-                <div class="menus">
-                    <select id="lieu" name="lieu">
-                        <option value="lieu">Lieu</option>
-                        <option value="ville">Ville</option>
-                        <option value="lyon">Lyon</option>
-                        <option value="marseille">Marseille</option>
-                        <option value="bordeaux">Bordeaux</option>
-                    </select>
-            
-                    <select id="domaine" name="domaine">
-                        <option value="domaine">Domaine</option>
-                        <option value="generaliste">Géneraliste</option>
-                        <option value="btp">BTP</option>
-                        <option value="sys">Systèmes embarqués</option>
-                    </select>
-            
-                    <select id="competences" name="competences">
-                        <option value="competances">Compétences</option>
-                        <option value="html">HTML</option>
-                        <option value="css">CSS</option>
-                        <option value="javascript">JavaScript</option>
-                        <option value="python">Python</option>
-                    </select>
-                </div>
-
+        <section class="criteres">
+            <div class="search">
+                <form action="recherche.php" method="GET" class="search-bar">
+                    <input placeholder="Recherche"  type="text" name="query"/>
+                </form>
+            </div>
             </section>
+            <div class="menus"> <!-- Menus déroulants -->
+                <form action="recherche.php" method="GET">
+                    <select id="ville" name="ville">
+                        <option value="">Ville</option>
+                        <?php foreach ($villes as $nom_ville): ?>
+                            <option value="<?php echo $nom_ville; ?>"><?php echo $nom_ville; ?></option>
+                        <?php endforeach; ?>
+                    </select>
+                    <select id="secteur" name="secteur">
+                        <option value="">Secteur</option>
+                        <?php foreach ($secteurs as $secteur): ?>
+                            <option value="<?php echo $secteur; ?>"><?php echo $secteur; ?></option>
+                        <?php endforeach; ?>
+                    </select>
+                    <select id="competences" name="competences">
+                        <option value="">Compétences</option>
+                        <?php foreach ($comp_requises as $comp): ?>
+                            <option value="<?php echo $comp; ?>"><?php echo $comp; ?></option>
+                        <?php endforeach; ?>
+                    </select>
+                    <select id="entreprises" name="entreprises"> <!-- Utilisation des noms des entreprises récupérés depuis la nouvelle fonction -->
+                        <option value="">Entreprises</option>
+                        <?php foreach ($entreprises as $entreprise): ?>
+                            <option value="<?php echo $entreprise; ?>"><?php echo $entreprise; ?></option>
+                        <?php endforeach; ?>
+                    </select>
+                </form>
+            </div>
         </section>
         
         <section class="bottom">
             <?php foreach ($offres as $offre_stage): ?>
                 <div class="offre">
-                <a href="offres.php?id=<?php echo $offre_stage['id_offre']; ?>" class="offre-link">
-                <div class="midle_content">
-                <h1 class="intitule"><?php echo $offre_stage['intitule']; ?></h1>
-                <p class="info">Niveau requis du poste : <span class="niveau_requis"><?php echo $offre_stage['niveau_requis']; ?></span></p>
-                <p class="info">Secteur du poste : <span class="secteur"><?php echo $offre_stage['nom_secteur']; ?></span></p>
-                <p class="info">Nombres de places du poste : <span class="nbr_places"><?php echo $offre_stage['nbr_places']; ?></span></p>
-                <p class="info">Compétences requises du poste : <span class="comp_requises"><?php echo $offre_stage['comp_requises']; ?></span></p>
-            </div>
-                </a>
+                    <a href="offres.php?id=<?php echo $offre_stage['id_offre']; ?>" class="offre-link">
+                        <div class="midle_content">
+                            <h1 class="intitule"><?php echo $offre_stage['intitule']; ?></h1>
+                            <p class="info">Nom de l'entreprise : <span class="nom_ent"><?php echo $entreprise ?></span></p>
+                            <p class="info">Niveau requis du poste : <span class="niveau_requis"><?php echo $offre_stage['niveau_requis']; ?></span></p>
+                            <p class="info">Secteur du poste : <span class="secteur"><?php echo $offre_stage['nom_secteur']; ?></span></p>
+                            <p class="info">Nombres de places du poste : <span class="nbr_places"><?php echo $offre_stage['nbr_places']; ?></span></p>
+                            <p class="info">Compétences requises du poste : <span class="comp_requises"><?php echo $offre_stage['comp_requises']; ?></span></p>
+                        </div>
+                    </a>
                 </div>
             <?php endforeach; ?>
-        </section>        
+        </section>
+
+        
     </section>
 
 </body>
---
+++ View/PHP/rechercheE.php
@@ -0,0 +1,101 @@
+<?php
+require_once '../../controller/controlrechercheE.php';
+require_once 'connexiondb.php';
+session_start();
+if (isset($_GET['idper']) || isset($_GET['nom']) || isset($_GET['prenom']) || isset($_GET['email'])) {
+    $idper = isset($_GET['idper']) ? $_GET['idper'] : null;
+    $nom = isset($_GET['nom']) ? $_GET['nom'] : null;
+    $prenom = isset($_GET['prenom']) ? $_GET['prenom'] : null;
+    $email = isset($_GET['email']) ? $_GET['email'] : null;
+    $etudiant = insertER($pdo,$email,$nom,$prenom,$idper);
+} else {
+    $etudiant = insertE($pdo);
+}
+
+
+ 
+    
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LightweightJobs</title>
+    <link rel="stylesheet" href="../css/accueil.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@400;600&display=swap">
+</head>
+
+<body>
+
+    
+    <header class="header">        
+
+        <div class="logo">
+            <a href="indexnv.html">
+                <img src="../img/logopngcrop.png" alt="Logo">
+            </a>
+        </div>
+        <div class="account">
+            <a class="account" href="dashboard.html">COMPTE</a>
+        </div>
+
+    </header>
+    
+    <section class="container">
+
+        <section class="top"> 
+       
+            <div class="menus"> <!-- Menus déroulants -->
+                <form action="rechercheE.php" method="GET">
+                <select id="nom" name="nom" >
+                    <option value="">Nom</option>
+                    <?php foreach ($etudiant  as $etudiants): ?>
+                        <option value="<?php echo $etudiants['nom']; ?>"><?php echo $etudiants['nom']; ?></option>
+                    <?php endforeach; ?>
+                </select>
+
+                
+                <select id="prenom" name="prenom">
+                    <option value="">Prénom</option>
+                    <?php foreach ($etudiant as $etudiants): ?>
+                        <option value="<?php echo $etudiants['prenom']; ?>"><?php echo $etudiants['prenom']; ?></option>
+                    <?php endforeach; ?>
+                </select>
+
+               
+                <select id="email" name="email">
+                    <option value="">Adresse e-mail</option>
+                    <?php foreach ($etudiant as $etudiants): ?>
+                        <option value="<?php echo $etudiants['email']; ?>"><?php echo $etudiants['email']; ?></option>
+                    <?php endforeach; ?>
+                </select>
+                <input type="submit" value="Rechercher" style="margin-left:4vw;border-radius:4vw;heigth:5vh;padding:1%;cursor:pointer;">
+                </form>
+            </div>
+        </section>
+        
+        <section class="bottom">
+        <?php foreach ($etudiant as $etudiants): ?>
+                <div class="offre">
+                <a href="rechercheE.php?idper=<?php echo $etudiants['idper']; ?>" class="offre-link" >
+                        <div class="midle_content" >
+                            <h1 class="intitule">Etudiant</h1>
+                            <input type="hidden" name="idper" value="<?php echo $etudiants['idper']; ?>">
+                            <p class="info">Nom de l'étudiant :  <?php echo $etudiants['nom']; ?></p>
+                            <p class="info">Prenom de l'étudiant :  <?php echo $etudiants['prenom'] ?></p>
+                            <p class="info">email:  <?php echo $etudiants['email']; ?></p>
+                        </div>
+                       
+                    </a>
+                    <button type="button" onclick="window.location.href = 'modifE.php?idper=' +<?php echo $etudiants['idper']?>;">modifier</button>
+                </div>
+            <?php endforeach; ?>
+        </section>
+        
+   
+
+</body>
+</html>
